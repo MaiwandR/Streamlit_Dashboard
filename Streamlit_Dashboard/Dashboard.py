cd22eb292e7ca60b83aad9356ad7f1a5d5522732
MaiwandR <114307104+MaiwandR@users.noreply.github.com>
2024-09-30 22:23:47 -0400

    Update Dashboard.py

--- Streamlit_Dashboard/Dashboard.py
+++ Streamlit_Dashboard/Dashboard.py
@@ -16,7 +16,7 @@ if fl is not None:
     st.write(filename)
     df = pd.read_csv(filename)
 else:
-    os.chdir(r"Streamlit_Dashboard")
+    os.chdir(r"Streamlit_Dashboard/Streamlit_Dashboard")
     df = pd.read_csv("Sample - Superstore.csv")
 
 col1, col2 = st.columns((2))
