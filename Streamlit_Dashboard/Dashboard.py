e0f669bd65553724c51d2aa6483a5517eb412927
MaiwandR <114307104+MaiwandR@users.noreply.github.com>
2025-03-07 08:10:17 -0500

    Update Dashboard.py

--- Streamlit_Dashboard/Dashboard.py
+++ Streamlit_Dashboard/Dashboard.py
@@ -16,7 +16,7 @@ if fl is not None:
     st.write(filename)
     df = pd.read_csv(filename)
 else:
-    df = pd.read_csv("Sample - Superstore.csv")
+    df = pd.read_csv("Streamlit_Dashboard/Sample - Superstore.csv")
 
 col1, col2 = st.columns((2))
 df["Order Date"] = pd.to_datetime(df["Order Date"])
